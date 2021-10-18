366ac964f9a04b8eabd1fa406247820536fadfd9
jsg5383 <jsg5383@gmail.com>
2021-10-18 23:56:00 +0900

    시작!!!!!!!!!!!!

---
+++ src/main/java/com/firstlab/study/controller/indexController.java
@@ -0,0 +1,34 @@
+package com.firstlab.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class indexController {
+
+    @GetMapping("/{boolean}")
+    public String index(@PathVariable("boolean") String paramTrue, Model model) {
+
+        Boolean isTrue = false;
+        if (paramTrue.equals("true")) {
+            isTrue = true;
+        }
+
+        Integer money = 1000;
+
+        if (isTrue) {
+            money = money + 500;
+        } else {
+            money = money + 300;
+        }
+
+        System.out.println(money);
+
+        model.addAttribute("value", 1500);
+
+        return "index";
+    }
+
+}
