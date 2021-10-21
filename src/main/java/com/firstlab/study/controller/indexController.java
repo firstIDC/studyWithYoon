1601c844d05c1177636d47a443fbaa7488b15738
dhflhip <dhflhip.yun@gmail.com>
2021-10-21 23:00:56 +0900

    no message

--- src/main/java/com/firstlab/study/controller/indexController.java
+++ src/main/java/com/firstlab/study/controller/indexController.java
@@ -13,18 +13,22 @@ public class indexController {
             @RequestParam(value = "msg") String msg,
             @RequestParam(value = "msg2") String msg2,
             @RequestParam(value = "msg3") String msg3,
+            @RequestParam(value = "msg4") String msg4,
+            @RequestParam(value = "msg5") String msg5,
             Model model) {
 
-        msg = "111";
-        msg2 = "222";
-        msg3 = "333";
+        msg = "서른 마흔 다섯살이다";
+        msg2 = "예술적으로 가셨구만";
+        msg3 = "내가 아는 타짜 중에 최고였어요";
+        msg4 = "남조선 아새끼들 눈치가 얼마나 빠른데";
+        msg5 = "오잉";
 
-
-        model.addAttribute("msg", msg);
-        model.addAttribute("msg2", msg2);
-        model.addAttribute("msg3", msg3);
+        model.addAttribute("msg",msg);
+        model.addAttribute("msg2",msg2);
+        model.addAttribute("msg3",msg3);
+        model.addAttribute("msg4",msg4);
+        model.addAttribute("msg5",msg5);
 
         return "index";
-    }
-
+}
 }
